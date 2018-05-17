c769d85446f2ef38c8e51d528875d9b3a6d00429
juanonsoftware <juanonsoftware@github.com>
2018-05-17 13:56:57 +0700

    Adding Chromium to the list of common software

--- Libs/ChocoInstallCommonSoftwareForServer.ps1
+++ Libs/ChocoInstallCommonSoftwareForServer.ps1
@@ -3,9 +3,11 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 # 7Zip - https://chocolatey.org/packages/7zip
 choco install 7zip -y
 
-#Notepad++ - https://chocolatey.org/packages/notepadplusplus.install
+# Notepad++ - https://chocolatey.org/packages/notepadplusplus.install
 choco install notepadplusplus.install -y
 
 # TreeSize Free https://chocolatey.org/packages/treesizefree
 choco install treesizefree -y
 
+# Chromium
+choco install chromium -y
