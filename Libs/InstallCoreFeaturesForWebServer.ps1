c66c8427d7e5370762ef3d5f95ff76a532c2ced7
juanonsoftware <juanonsoftware@github.com>
2018-06-08 16:33:40 +0700

    Separate scripts

--- Libs/InstallCoreFeaturesForWebServer.ps1
+++ Libs/InstallCoreFeaturesForWebServer.ps1
@@ -8,31 +8,14 @@ Install-WindowsFeature -Name Web-Http-Tracing
 Write-Host "Installing Web-Basic-Auth"
 Install-WindowsFeature -Name Web-Basic-Auth -IncludeManagementTools
 
-# Change the value to make it download packages from Microsoft Update
-$au = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU'
-if ($au.UseWUServer -eq 1) {
-	Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU' -Name UseWUServer -Value 0
-	Restart-Service -Name wuauserv
-}
+# Install the component requires by applications
+Write-Host "Installing Telnet-Client"
+Install-WindowsFeature Telnet-Client
 
-Write-Host "Installing NET-Framework-Core"
-Install-WindowsFeature -Name NET-Framework-Core
-
-if ($au.UseWUServer -eq 1) {
-	Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU' -Name UseWUServer -Value 1
-	Restart-Service -Name wuauserv
-}
+# Include .Net components
+(Get-Item -Path ".\").FullName + "\InstallNetFrameworks.ps1"
 
 #Install and start Web Management Service
 Write-Host "Installing Web-Mgmt-Service"
 Install-WindowsFeature -Name Web-Mgmt-Service -IncludeManagementTools
 Start-Service -Name WMSVC
-
-# Install the component requires by applications
-Write-Host "Installing web-asp-net45"
-Install-WindowsFeature -Name web-asp-net45 -IncludeManagementTools
-
-
-# Install the component requires by applications
-Write-Host "Installing Telnet-Client"
-Install-WindowsFeature Telnet-Client
